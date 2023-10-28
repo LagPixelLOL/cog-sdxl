4c74e0c13192e67406adfc4a24334593e9baeb32
v2ray <2282688304@qq.com>
2023-10-28 07:55:48 +0000

    Added VAE selection.

--- predict.py
+++ predict.py
@@ -4,38 +4,43 @@ import os
 import glob
 import torch
 import diffusers
-from diffusers import StableDiffusionXLPipeline
-from PIL import Image
+from diffusers import StableDiffusionXLPipeline, AutoencoderKL
 
 def find_models(models_dir):
-    model_names = []
-    safetensor_files = glob.glob(f"{models_dir}/**/*.safetensors", recursive=True)
-    ckpt_files = glob.glob(f"{models_dir}/**/*.ckpt", recursive=True)
-
-    for file in safetensor_files:
-        if not file.endswith(".vae.safetensors"):
-            model_names.append(os.path.basename(file))
-    for file in ckpt_files:
-        if not file.endswith(".vae.ckpt"):
-            model_names.append(os.path.basename(file))
-
+    model_names = [os.path.basename(file) for file in glob.glob(f"{models_dir}/**/*.safetensors", recursive=True) + glob.glob(f"{models_dir}/**/*.ckpt", recursive=True)]
     return model_names
 
-MODELS_DIR_PATH = "models"
-MODEL_NAMES = find_models(MODELS_DIR_PATH)
-assert(len(MODEL_NAMES) > 0)
+def find_vaes(vaes_dir):
+    vae_names = []
+    for folder in os.listdir(vaes_dir):
+        folder_path = os.path.join(vaes_dir, folder)
+        if os.path.isdir(folder_path):
+            safetensors_file = os.path.join(folder_path, 'diffusion_pytorch_model.safetensors')
+            bin_file = os.path.join(folder_path, 'diffusion_pytorch_model.bin')
+            config_file = os.path.join(folder_path, 'config.json')
+            if (os.path.isfile(safetensors_file) or os.path.isfile(bin_file)) and os.path.isfile(config_file):
+                vae_names.append(folder)
+    return vae_names
 
+MODELS_DIR_PATH = "models"
+VAES_DIR_PATH = "vaes"
+MODEL_NAMES = find_models(MODELS_DIR_PATH)
+assert len(MODEL_NAMES) > 0, f"You don't have any model under \"{MODELS_DIR_PATH}\", please put at least 1 model in there."
+VAE_NAMES = find_vaes(VAES_DIR_PATH)
+assert len(VAE_NAMES) > 0, f"You don't have any VAE under \"{VAES_DIR_PATH}\", please put at least 1 VAE in there, you can run \"python3 -c 'from huggingface_hub import snapshot_download as d;d(repo_id=\"madebyollin/sdxl-vae-fp16-fix\", allow_patterns=[\"config.json\", \"diffusion_pytorch_model.safetensors\"], local_dir=\"./vaes/sdxl-vae-fp16-fix\", local_dir_use_symlinks=False)'\" to download a fp16 fixed default SDXL VAE if you don't know what to use."
+
+# Cog will only run this class in a single thread
 class Predictor(BasePredictor):
 
     def setup(self):
-        self.pipelines = SDXLMultiPipelineSwitchAutoDetect(MODELS_DIR_PATH, MODEL_NAMES)
-        if not os.path.exists("tmp"):
-            os.makedirs("tmp")
+        self.pipelines = SDXLMultiPipelineSwitchAutoDetect(MODELS_DIR_PATH, MODEL_NAMES, VAES_DIR_PATH, VAE_NAMES)
+        os.makedirs("tmp", exist_ok=True)
 
-    @torch.inference_mode
+    @torch.no_grad
     def predict(
         self,
         model: str = Input(description="The model to use", default=MODEL_NAMES[0], choices=MODEL_NAMES),
+        vae: str = Input(description="The VAE to use", default=VAE_NAMES[0], choices=VAE_NAMES),
         prompt: str = Input(description="The prompt", default="catgirl, cat ears, white hair, golden eyes, bob cut, pov, face closeup, smile"),
         negative_prompt: str = Input(description="The negative prompt (For things you don't want)", default="lowres, low quality, worse quality, monochrome, blurry, headphone, big breasts"),
         steps: int = Input(description="The steps when generating", default=35, ge=1, le=100),
@@ -46,7 +51,7 @@ class Predictor(BasePredictor):
         batch_size: int = Input(description="Number of images to generate (1-4)", default=1, ge=1, le=4),
         seed: int = Input(description="The seed used when generating, set to -1 for random seed", default=-1),
     ) -> List[Path]:
-        pipeline = self.pipelines.get_pipeline(model)
+        pipeline = self.pipelines.get_pipeline(model, vae)
         generator = None
         if seed != -1:
             generator = torch.Generator(device="cuda").manual_seed(seed)
@@ -63,28 +68,37 @@ class Predictor(BasePredictor):
 
         return image_paths
 
-class SDXLMultiPipelineSwitchAutoDetect():
+class SDXLMultiPipelineSwitchAutoDetect:
 
-    def __init__(self, models_dir_path, model_names):
+    def __init__(self, models_dir_path, model_names, vaes_dir_path, vae_names):
         self.models_dir_path = models_dir_path
-        self.model_pipeline_dict = {model: None for model in model_names}
-        self.vae = diffusers.AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.bfloat16)
-        self.vae.enable_slicing()
-        self.vae.enable_tiling()
+        self.model_pipeline_dict = {model_name: None for model_name in model_names}
+        self.vaes_dir_path = vaes_dir_path
+        self.vae_obj_dict = {vae_name: None for vae_name in vae_names}
         self._load_all_models()
+        self._load_all_vaes()
         self.on_cuda_model = model_names[0]
-        self.model_pipeline_dict[self.on_cuda_model].to("cuda")
+        on_cuda_pipeline = self.model_pipeline_dict[self.on_cuda_model]
+        on_cuda_pipeline.to("cuda")
+        on_cuda_pipeline.vae = self.vae_obj_dict[vae_names[0]]
 
-    def get_pipeline(self, model_name):
+    def get_pipeline(self, model_name, vae_name):
         pipeline = self.model_pipeline_dict.get(model_name)
+        vae = self.vae_obj_dict.get(vae_name)
+        # __init__ function guarantees models and VAEs to be loaded.
         if pipeline is None:
-            raise ValueError(f"Model '{model_name}' not found or not loaded.")
+            raise ValueError(f"Model '{model_name}' not found.")
+        if vae is None:
+            raise ValueError(f"VAE '{vae_name}' not found.")
 
         if model_name != self.on_cuda_model:
-            self.model_pipeline_dict[self.on_cuda_model].to("cpu")
+            on_cuda_pipeline = self.model_pipeline_dict[self.on_cuda_model]
+            on_cuda_pipeline.vae = None
+            on_cuda_pipeline.to("cpu")
             pipeline.to("cuda")
             self.on_cuda_model = model_name
 
+        pipeline.vae = vae
         return pipeline
 
     def _load_all_models(self):
@@ -93,10 +107,20 @@ class SDXLMultiPipelineSwitchAutoDetect():
             self.model_pipeline_dict[model_name] = pipeline
 
     def _load_model(self, model_name):
-        model_path = os.path.join(self.models_dir_path, model_name)
-        pipeline = StableDiffusionXLPipeline.from_single_file(
-            model_path, vae=self.vae, torch_dtype=torch.bfloat16, variant="fp16", add_watermarker=False,
-        )
+        pipeline = StableDiffusionXLPipeline.from_single_file(os.path.join(self.models_dir_path, model_name), torch_dtype=torch.bfloat16, variant="fp16", add_watermarker=False)
+        pipeline.vae = None
         pipeline.scheduler = diffusers.UniPCMultistepScheduler.from_config(pipeline.scheduler.config)
         pipeline.enable_xformers_memory_efficient_attention()
         return pipeline
+
+    def _load_all_vaes(self):
+        for vae_name in self.vae_obj_dict.keys():
+            vae = self._load_vae(vae_name)
+            self.vae_obj_dict[vae_name] = vae
+
+    def _load_vae(self, vae_name):
+        vae = AutoencoderKL.from_pretrained(os.path.join(self.vaes_dir_path, vae_name), torch_dtype=torch.bfloat16)
+        vae.enable_slicing()
+        vae.enable_tiling()
+        vae.to("cuda")
+        return vae
